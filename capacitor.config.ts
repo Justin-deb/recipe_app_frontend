29259ebf547d48d79c40803bc48caf68e3c2a04d
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-07-01 15:34:32 -0600

    Added Capacitor, implemented on June 17th 2026

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'recipe_app_frontend',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/pages/favorites.tsx
@@ -0,0 +1,30 @@
+// import { useLocalStorage } from "@uidotdev/usehooks";
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router";
+// import type { UserSession } from "../types/user-session";
+
+export const Favorites = () => {
+  // const userSession = useLocalStorage<UserSession>("user_session");
+  // const [userId, setUserId] = useState<number>();
+  // useEffect(() => {
+  //   setUserId(await getAllUser)
+  // }, []);
+  return (
+    // <div
+    //   style={{
+    //     border: '1px solid #ccc',
+    //     borderRadius: '8px',
+    //     padding: '16px',
+    //     marginBottom: '12px',
+    //   }}
+    // >
+    //   <h2>{recipeName}</h2>
+
+    //   <Link to={`${route}/${recipeId}`}>
+    //     Ver receta
+    //   </Link>
+    // </div>
+    <>
+    </>
+  );
+}
